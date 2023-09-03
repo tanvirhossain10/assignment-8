64bb5c81db6b111af5fd68c507249c9ac1715fba
tanvirhossain10 <tanvirhossen808@gmail.com>
2023-09-03 11:31:32 +0600

    added assingmetn detials and first commit

--- src/App.js
+++ src/App.js
@@ -1,86 +1,22 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h2>he</h2>
-//       <Friends name="alom" age="30"></Friends>
-//     </div>
-//   );
-// }
-
-// function Friends(Props) {
-//   console.log(Props)
-//   return (
-//     <div>
-//       <h3>Name:{Props.name}</h3>
-//       {/* <h5>Age:</h5> */}
-//     </div>
-//   )
-// }
-// export default App;
-
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import BlogHead from './blog/Blog-heading/BlogHead';
-import Mob from './mobile/Mobile/Mob';
-import Json from './json/Json/Json';
 
 function App() {
-  // const nayoks = ['rubel', 'kajols', 'feardos', 'shH']
-
-  const nayoks = ['rubel']
   return (
     <div className="App">
-      <Json></Json>
-      <Mob></Mob>
-      <BlogHead heading="Asome Blogs" author='I am the heading author'></BlogHead>
-      <h2>he</h2>
-      {/* <Friends></Friends> */}
-      {
-        nayoks.map(nayok => <Friends name={nayok}></Friends>)
-      }
-      {/* <Friends name="alom" age="30"></Friends>
-      <Friends name="alom" age="30"></Friends> */}
-      <Blogs></Blogs>
+      <img src="https://i.postimg.cc/W3YL2F7H/gettyimages-174688675-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/T2Z2mFrH/gettyimages-637216704-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/0N29Pssr/gettyimages-694198404-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/Y2gBc9Wb/gettyimages-746028473-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/5NVdfV51/gettyimages-807031028-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/vZ0p7xJH/gettyimages-872841180-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/d1ypKHLL/gettyimages-922684138-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/VL1T3F1k/gettyimages-996979970-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/d3FHcJbx/gettyimages-1254672762-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/fTBH1nB8/gettyimages-1295796231-612x612.jpg" alt="" />
+      <img src="https://i.postimg.cc/W3YL2F7H/gettyimages-174688675-612x612.jpg" alt="" />
     </div>
   );
-}
-// console.log(props);
-
-function Friends(props) {
-  const [agei, setBo
-  ] = useState(0);
-  // console.log(u)
-
-  const age = () => { setBo(agei + 1) }
-
-
-  console.log(props);
-  return (
-    <div style={{ backgroundColor: 'red', margin: '20px', borderRadius: '20px' }}>
-      <h3>Name: {props.name}</h3>
-      <h5>Age: {props.age}</h5>
-      console.log({agei});
-      <h5>Age:{agei}</h5>
-      <button onClick={age}>Increase</button>
-    </div>
-  );
-}
-
-function Blogs() {
-  return (
-    <div>
-      <article className='blog'>
-        <h2 style={{ color: 'black', textAlign: 'start', marginTop: '10px' }}>You can know many thing from the Blogs</h2>
-        <p style={{ display: 'inline' }}>This is the paragraph tag. </p>
-        This the article of some famous person in BD.Bangladesh is a very poor country in the world.Where every country in the world are very rich where bangladesh is very poor.This is the true thing .If you love true then you have to accept the true otherwise you will suffer in your day to day life.So don't worry about that.
-      </article>
-    </div >
-  )
 }
 
 export default App;
-
