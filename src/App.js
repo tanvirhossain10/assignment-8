c6e78e02b0c60922ac93febb195952b092fd66bc
tanvirhossain10 <tanvirhossen808@gmail.com>
2023-09-03 12:07:30 +0600

    added header site

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <img src="https://i.postimg.cc/W3YL2F7H/gettyimages-174688675-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/T2Z2mFrH/gettyimages-637216704-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/0N29Pssr/gettyimages-694198404-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/Y2gBc9Wb/gettyimages-746028473-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/5NVdfV51/gettyimages-807031028-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/vZ0p7xJH/gettyimages-872841180-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/d1ypKHLL/gettyimages-922684138-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/VL1T3F1k/gettyimages-996979970-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/d3FHcJbx/gettyimages-1254672762-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/fTBH1nB8/gettyimages-1295796231-612x612.jpg" alt="" />
-      <img src="https://i.postimg.cc/W3YL2F7H/gettyimages-174688675-612x612.jpg" alt="" />
+    <div>
+      <Header></Header>
     </div>
   );
 }
